dfbcde752cb5145e235466ce85d64d77f2daadf5
Caibin Chen <tigersoldi@gmail.com>
2017-10-05 21:59:38 +0800

    Fix package metadata

--- lsp-javacomp.el
+++ lsp-javacomp.el
@@ -18,6 +18,19 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; A lsp-mode client that provides Java code-completion and other IDE
+;; features for Emacs. It's backed by JavaComp.
+
+;; To use it, add the code below to your .emacs file:
+
+;;   (with-eval-after-load 'lsp-mode
+;;     (require 'lsp-javacomp))
+;;   (add-hook 'java-mode-hook 'lsp-mode)
+
+;;; Code:
+
 (require 'cc-mode)
 (require 'lsp-mode)
 
